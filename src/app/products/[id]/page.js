c00aa9206bddd893301f1b00adee763197cbc419
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-01-11 21:46:04 +0600

    Dynamic route added

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const Product = () => {
-    return (
-        <div>
-            This is Dynamic Product!!!
-        </div>
-    );
+const Product = ({ params, searchParams }) => {
+  console.log(params, searchParams);
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">
+        This is Dynamic Product - {params?.id}
+      </h1>
+      <p>Category = {searchParams?.category}</p>
+    </div>
+  );
 };
 
 export default Product;
