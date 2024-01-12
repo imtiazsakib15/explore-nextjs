c153db540d6870fcbd7c7f87ef97b462a5340ade
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-01-12 15:51:29 +0600

    Not found page added

---
+++ src/app/not-found.js
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <h1 className="text-3xl font-bold">404!!! This page is not found!</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
