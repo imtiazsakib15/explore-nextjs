df83224e913dc48e39273d293cd4fbd33a81a88b
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-01-12 15:58:58 +0600

    Error page added

---
+++ src/app/error.js
@@ -0,0 +1,13 @@
+"use client";
+
+const ErrorPage = () => {
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-red-500">
+        Something Went Wrong!!!
+      </h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
