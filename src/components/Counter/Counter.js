b6a71b10489209b58907107780effa486ae7832f
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-01-12 07:49:43 +0600

    Client side component created

---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,26 @@
+"use client";
+import { useState } from "react";
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+  console.log(counter);
+  return (
+    <div>
+      <h2 className="text-3xl font-bold">counter = {counter}</h2>
+      <button
+        className="px-4 py-2 border"
+        onClick={() => setCounter(counter + 1)}
+      >
+        Increase
+      </button>
+      <button
+        className="px-4 py-2 border"
+        onClick={() => setCounter(counter - 1)}
+      >
+        Decrease
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
