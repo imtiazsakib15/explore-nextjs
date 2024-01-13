95847366d36d158ed1c6794e7560d944e88df494
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-01-13 23:57:19 +0600

    Fetch data from server

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -3,10 +3,24 @@ export const metadata = {
   description: "This is blog page",
 };
 
-const BlogPage = () => {
+const BlogPage = async () => {
+  const res = await fetch("http://localhost:5000/posts", {
+    cache: "force-cache",
+  });
+  const blogs = await res.json();
+  // console.log(blogs);
+
   return (
-    <div>
-      <h1>This is Blog Page</h1>
+    <div className="max-w-5xl mx-auto">
+      <h1 className="text-4xl font-bold text-center py-6">Blogs</h1>
+      <div className="grid sm:grid-cols-2 gap-6">
+        {blogs?.map((blog) => (
+          <div className="bg-slate-700 rounded p-5 text-white" key={blog.id}>
+            <h3 className="text-2xl font-semibold">{blog.title}</h3>
+            <p>{blog.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
