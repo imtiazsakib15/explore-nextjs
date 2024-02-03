c98d7894a02cd72851b1d980723f877ce6815ef4
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-02-03 23:44:36 +0600

    Active link design added

---
+++ src/components/NavLinks/NavLinks.jsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const NavLinks = () => {
+  const pathname = usePathname();
+
+  const links = [
+    { pathname: "Home", route: "/" },
+    { pathname: "Album", route: "/album" },
+    { pathname: "About", route: "/about" },
+    { pathname: "Products", route: "/products" },
+    { pathname: "Blogs", route: "/blogs" },
+    { pathname: "Contact", route: "/contact" },
+    { pathname: "Projects", route: "/projects" },
+    { pathname: "Skills", route: "/skills" },
+  ];
+  return (
+    <>
+      {links.map((link) => {
+        const isActive = pathname.startsWith(link.route);
+        return (
+          <Link
+            key={link.pathname}
+            className={
+              isActive
+                ? "px-4 py-2 bg-green-600 text-white"
+                : "px-4 py-2 bg-slate-200"
+            }
+            href={link.route}
+          >
+            {link.pathname}
+          </Link>
+        );
+      })}
+    </>
+  );
+};
+
+export default NavLinks;
