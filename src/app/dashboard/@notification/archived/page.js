4df9483380742648cf65ce392f4096ae6279a98e
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-02-06 12:52:05 +0600

    Handled 404 error in parallel route

--- src/app/dashboard/@notification/archived/page.js
+++ src/app/dashboard/@notification/archived/page.js
@@ -1,19 +1,19 @@
 import Link from "next/link";
 
-const Notification = () => {
+const ArchivedNotification = () => {
   return (
     <div>
-      <h1 className="text-2xl font-bold p-8">Notification</h1>{" "}
+      <h1 className="text-2xl font-bold p-8">Archived Notification</h1>
       <button className="py-12">
         <Link
           className="text-blue-700 underline font-semibold"
-          href={"/dashboard/archived"}
+          href={"/dashboard"}
         >
-          Archived Notification
+          Default
         </Link>
       </button>
     </div>
   );
 };
 
-export default Notification;
+export default ArchivedNotification;
